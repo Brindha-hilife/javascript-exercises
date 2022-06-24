0b604e17ceb9959e8f1a48b20d04ded9ce560eae
Brindha-hilife <bindu.hilifeai@gmail.com>
2022-06-24 12:18:47 +0530

    Add function in tempConversion.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
 const ftoc = function() {
-
+  var temprature = 32;
+  var celsius = parseFloat(temprature-32)/1.8;
+  return celsius;
 };
 
 const ctof = function() {
-
+  var degree = 0;
+  var fehrenheit = parseFloat((degree*1.8)+32);
+  return fehrenheit;
 };
 
 // Do not edit below this line
