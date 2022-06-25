e0cd310004deeb8fb78d3aa6c1dd7329c7748792
Brindha-hilife <bindu.hilifeai@gmail.com>
2022-06-25 10:29:22 +0530

    Add function to sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,11 @@
 const sumAll = function() {
-    
-}
+  var min = 1;
+  var max = 4;
+  var sum = 0;
+  for(var i = min; i <= max; i++)
+  sum = sum + i;
+  return sum;
+ }
 
 // Do not edit below this line
 module.exports = sumAll;
