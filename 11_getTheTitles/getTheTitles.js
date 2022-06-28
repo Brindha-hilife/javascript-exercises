db50af257362a4e84f8b7a45d4ac9db232fa1ef3
Brindha-hilife <bindu.hilifeai@gmail.com>
2022-06-28 17:16:14 +0530

    Add function in getTheTitles.spec.js

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,17 @@
 const getTheTitles = function() {
-
+    const books = [
+        {
+          title: 'Book',
+          author: 'Name'
+        },
+        {
+          title: 'Book2',
+          author: 'Name2'
+        }
+      ]
+      var result;
+      return result = books.map(a => a.title);
+  
 };
 
 // Do not edit below this line
